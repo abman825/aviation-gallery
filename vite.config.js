43e8758dab6618ccc01b865acc03b6d8d9002dc8
abman825 <abrhamman825@gmail.com>
2026-03-09 23:41:57 +0300

    fix file names to index.html

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aviation-gallery/', 
+// ከዚህ በፊት የነበረውን አጥፍተህ ይህንን ሞክር
+base: './', 
 })

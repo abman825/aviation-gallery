2962f6f66b354d07e93eb9864a4d84725577b458
abman825 <abrhamman825@gmail.com>
2026-03-18 21:28:29 +0300

    Add endpoint path to API URL

--- server/server.mjs
+++ server/server.mjs
@@ -76,7 +76,7 @@ app.post('/api/orders', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);

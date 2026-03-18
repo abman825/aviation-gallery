49939d2e71c1044a4ea4f10d9f43fec0fec3c585
abman825 <abrhamman825@gmail.com>
2026-03-18 20:30:48 +0300

    Fix server reference error for Render

--- server/server.mjs
+++ server/server.mjs
@@ -78,6 +78,6 @@ app.post('/api/orders', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
-}); 
+});

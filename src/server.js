e41d1b62b142aa2232a7418bf8b246d166d170c6
abman825 <abrhamman825@gmail.com>
2026-03-20 11:52:19 +0300

    Fix database password and routes

--- src/server.js
+++ src/server.js
@@ -4,19 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-// ማሳሰቢያ፡ የ 'password' የሚለውን ቃል በእውነተኛው የዳታቤዝ ፓስወርድህ መተካትህን አረጋግጥ
-const mongoURI = "mongodb+srv://abrahamman825_db_user:Jl02Lx4pJWeiDZcr@cluster0.p76sc.mongodb.net/lilmooDB?retryWrites=true&w=majority&appName=Cluster0";
+// MongoDB Connection - ያቀረብከውን ትክክለኛ መረጃ ተጠቅሜያለሁ
+const mongoURI = "mongodb+srv://abrhamman825_db_user:Jl02Lx4pJWeiDZcr@cluster0.mongodb.net/aviation_gallery?retryWrites=true&w=majority";
+
 mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB ተገናኝቷል!'))
+  .then(() => console.log('MongoDB በትክክል ተገናኝቷል!'))
   .catch((err) => console.error('የዳታቤዝ ግንኙነት ስህተት:', err));
 
-// Order Schema
+// Order Schema - ትዕዛዞች የሚቀመጡበት መዋቅር
 const orderSchema = new mongoose.Schema({
   name: String,
   orderType: String,
@@ -28,10 +26,10 @@ const Order = mongoose.model('Order', orderSchema);
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Lilmoo Backend ሰርቨር እየሰራ ነው!');
+  res.send('Lilmoo Backend ሰርቨር በትክክል እየሰራ ነው!');
 });
 
-// ትዕዛዝ መቀበያ መንገድ (POST Route)
+// ትዕዛዝ መቀበያ መንገድ (ይህ ክፍል ነው ለውጥ የሚያመጣው)
 app.post('/api/orders', async (req, res) => {
   try {
     const { name, orderType } = req.body;
@@ -53,5 +51,5 @@ app.post('/api/orders', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`ሰርቨር በፖርት ${PORT} ላይ እየሰራ ነው`);
+  console.log(`ሰርቨር በፖርት ${PORT} ላይ ስራ ጀምሯል`);
 });

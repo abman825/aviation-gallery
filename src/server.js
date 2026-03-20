45cc120e988d8e756990b0643314b33120755d92
abman825 <abrhamman825@gmail.com>
2026-03-20 12:22:30 +0300

    Update dependencies

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection - ያቀረብከውን ትክክለኛ መረጃ ተጠቅሜያለሁ
-const mongoURI = "mongodb+srv://abrhamman825_db_user:Jl02Lx4pJWeiDZcr@cluster0.mongodb.net/aviation_gallery?retryWrites=true&w=majority";
+// MongoDB Connection
+const mongoURI = "mongodb+srv://abrhamman825_db_user:Jl02Lx4pJWeiDZcr@cluster0.p76sc.mongodb.net/aviation_gallery?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB በትክክል ተገናኝቷል!'))
   .catch((err) => console.error('የዳታቤዝ ግንኙነት ስህተት:', err));
 
-// Order Schema - ትዕዛዞች የሚቀመጡበት መዋቅር
+// Order Schema
 const orderSchema = new mongoose.Schema({
   name: String,
   orderType: String,
@@ -29,22 +31,13 @@ app.get('/', (req, res) => {
   res.send('Lilmoo Backend ሰርቨር በትክክል እየሰራ ነው!');
 });
 
-// ትዕዛዝ መቀበያ መንገድ (ይህ ክፍል ነው ለውጥ የሚያመጣው)
 app.post('/api/orders', async (req, res) => {
   try {
     const { name, orderType } = req.body;
-    
-    if (!name || !orderType) {
-      return res.status(400).json({ error: 'እባክህ ስም እና የልብስ አይነት አስገባ' });
-    }
-
     const newOrder = new Order({ name, orderType });
     await newOrder.save();
-    
-    console.log('አዲስ ትዕዛዝ ተመዝግቧል:', newOrder);
     res.status(201).json({ message: 'ትዕዛዝህ በትክክል ደርሷል!', data: newOrder });
   } catch (error) {
-    console.error('ትዕዛዝ ሲመዘገብ ስህተት ተከስቷል:', error);
     res.status(500).json({ error: 'ትዕዛዝ መላክ አልተቻለም' });
   }
 });
